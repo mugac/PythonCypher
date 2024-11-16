ce722b813c621b247a2b6341b6da9135790d6553
DEDSacram <bubles013@gmail.com>
2024-11-16 05:33:21 +0100

    db1 end1

---
+++ PROXY/redirectordb1.py
@@ -0,0 +1,111 @@
+import socket
+import threading
+import select
+import sys
+
+terminateAll = False
+
+class ClientThread(threading.Thread):
+    def __init__(self, clientSocket, targetHost, targetPort):
+        threading.Thread.__init__(self)
+        self.__clientSocket = clientSocket
+        self.__targetHost = targetHost
+        self.__targetPort = targetPort
+        
+    def run(self):
+        print("Client Thread started")
+        
+        self.__clientSocket.setblocking(0)
+        
+        targetHostSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        targetHostSocket.connect((self.__targetHost, self.__targetPort))
+        targetHostSocket.setblocking(0)
+        
+        clientData = b''
+        targetHostData = b''
+        terminate = False
+        while not terminate and not terminateAll:
+            inputs = [self.__clientSocket, targetHostSocket]
+            outputs = []
+            
+            if len(clientData) > 0:
+                outputs.append(self.__clientSocket)
+                
+            if len(targetHostData) > 0:
+                outputs.append(targetHostSocket)
+            
+            try:
+                inputsReady, outputsReady, errorsReady = select.select(inputs, outputs, [], 1.0)
+            except Exception as e:
+                print(f"Exception during select: {e}")
+                break
+                
+            for inp in inputsReady:
+                if inp == self.__clientSocket:
+                    try:
+                        data = self.__clientSocket.recv(4096)
+                    except Exception as e:
+                        print(f"Exception while receiving from client: {e}")
+                    
+                    if data:
+                        if len(data) > 0:
+                            targetHostData += data
+                        else:
+                            terminate = True
+                elif inp == targetHostSocket:
+                    try:
+                        data = targetHostSocket.recv(4096)
+                    except Exception as e:
+                        print(f"Exception while receiving from target host: {e}")
+                        
+                    if data:
+                        if len(data) > 0:
+                            clientData += data
+                        else:
+                            terminate = True
+                        
+            for out in outputsReady:
+                if out == self.__clientSocket and len(clientData) > 0:
+                    try:
+                        bytesWritten = self.__clientSocket.send(clientData)
+                        if bytesWritten > 0:
+                            clientData = clientData[bytesWritten:]
+                    except Exception as e:
+                        print(f"Exception while sending to client: {e}")
+                elif out == targetHostSocket and len(targetHostData) > 0:
+                    try:
+                        bytesWritten = targetHostSocket.send(targetHostData)
+                        if bytesWritten > 0:
+                            targetHostData = targetHostData[bytesWritten:]
+                    except Exception as e:
+                        print(f"Exception while sending to target host: {e}")
+        
+        self.__clientSocket.close()
+        targetHostSocket.close()
+        print("ClientThread terminating")
+
+if __name__ == '__main__':
+    # if len(sys.argv) != 5:
+    #     print('Usage:\n\tpython SimpleTCPRedirector <host> <port> <remote host> <remote port>')
+    #     print('Example:\n\tpython SimpleTCPRedirector localhost 8080 www.google.com 80')
+    #     sys.exit(0)        
+        
+    localHost = "10.128.40.94"
+    localPort = 51234
+    targetHost = "10.128.40.90"
+    targetPort = 3306
+        
+    serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    serverSocket.bind((localHost, localPort))
+    serverSocket.listen(5)
+    print("Waiting for client...")
+    while True:
+        try:
+            clientSocket, address = serverSocket.accept()
+        except KeyboardInterrupt:
+            print("\nTerminating...")
+            terminateAll = True
+            break
+        ClientThread(clientSocket, targetHost, targetPort).start()
+        
+    serverSocket.close()
